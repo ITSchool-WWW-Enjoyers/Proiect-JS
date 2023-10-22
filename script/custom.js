687c68c341981fcc29f19960483700290c9def98
victoria27199 <victoria.sarateanu1@gmail.com>
2023-10-22 20:38:37 +0300

    HTML form-submit

--- script/custom.js
+++ script/custom.js
@@ -1,28 +1,27 @@
-console.log("Verify connection with HTML!");
-
 const taskInput = document.getElementById('taskInput');
-const createTask = document.getElementById('createTask');
+const createTask = document.getElementById('taskForm');
 const taskList = document.getElementById('taskList');
 
-createTask.addEventListener('click', () => {
+createTask.addEventListener('submit', (event) => {
+    event.preventDefault(); 
     const taskContent = taskInput.value;
     if (taskContent) {
         const newTask = document.createElement('li');
         newTask.innerHTML =  `
             <input type='checkbox'>
             <span>${taskContent}</span>
-            <button class='edit'> Edit </button>
-            <button class='delete'> Delete</button>
+            <button class='edit'>Edit</button>
+            <button class='delete'>Delete</button>
         `;
         taskList.appendChild(newTask);
         taskInput.value = '';
     }
 });
 
-taskList.addEventListener('change', (ev) => {
-    if (ev.target.type === 'checkbox') {
-        const task = ev.target.nextElementSibling;
-        if (ev.target.checked) {
+taskList.addEventListener('change', (event) => {
+    if (event.target.type === 'checkbox') {
+        const task = event.target.nextElementSibling;
+        if (event.target.checked) {
             task.style.textDecoration = 'line-through';
             task.style.color = 'red';
         } else {
@@ -30,12 +29,11 @@ taskList.addEventListener('change', (ev) => {
             task.style.color = 'initial';
         }
     }
+  
 });
 
-taskList.addEventListener("click", (event) =>{
+taskList.addEventListener('click', (event) => {
     if (event.target.classList.contains('delete')) {
         event.target.parentElement.remove();
     }
 });
-
-
